443de900d0b7bbd35793ccaf6bee0bbd216a864d
Dmitrii Tuchs <dtuchs@gmail.com>
2023-03-18 00:39:58 +0600

    second commit

--- src/test/java/guru/qa/parsing/FileParsingTest.java
+++ src/test/java/guru/qa/parsing/FileParsingTest.java
@@ -3,16 +3,20 @@ package guru.qa.parsing;
 import com.codeborne.pdftest.PDF;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.xlstest.XLS;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.opencsv.CSVReader;
+import guru.qa.Human;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.shadow.com.univocity.parsers.csv.Csv;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Objects;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import static com.codeborne.selenide.Selenide.$;
 
@@ -53,5 +57,51 @@ public class FileParsingTest {
     }
   }
 
+  @Test
+  void filesEqulsTest() throws Exception {
+    Selenide.open("https://github.com/qa-guru/qa_guru_18_files/blob/master/src/test/resources/qaguru.csv");
+    File download = $("#raw-url").download();
+    try (InputStream isExpected = cl.getResourceAsStream("expectedfiles/qaguru.csv");
+        InputStream downloaded = new FileInputStream(download)) {
+      Assertions.assertEquals(
+         Objects.hash(isExpected.readAllBytes()),
+          Objects.hash( downloaded.readAllBytes())
+      );
+    }
+  }
 
+  @Test
+  void zipTest() throws Exception {
+    try (InputStream is = cl.getResourceAsStream("sample.txt.zip");
+         ZipInputStream zs = new ZipInputStream(is)) {
+      ZipEntry entry;
+      while ((entry = zs.getNextEntry()) != null) {
+        Assertions.assertTrue(entry.getName().contains("sample.txt"));
+      }
+    }
+  }
+
+  @Test
+  void jsonTest() throws Exception {
+    Gson gson = new Gson();
+    try (InputStream is = cl.getResourceAsStream("human.json");
+         InputStreamReader isr = new InputStreamReader(is)) {
+      JsonObject jsonObject = gson.fromJson(isr, JsonObject.class);
+
+      Assertions.assertTrue(jsonObject.get("isClever").getAsBoolean());
+      Assertions.assertEquals(33, jsonObject.get("age").getAsInt());
+    }
+  }
+
+  @Test
+  void jsonCleverTest() throws Exception {
+    Gson gson = new Gson();
+    try (InputStream is = cl.getResourceAsStream("human.json");
+         InputStreamReader isr = new InputStreamReader(is)) {
+      Human human = gson.fromJson(isr, Human.class);
+
+      Assertions.assertTrue(human.isClever);
+      Assertions.assertEquals(33, human.age);
+    }
+  }
 }
